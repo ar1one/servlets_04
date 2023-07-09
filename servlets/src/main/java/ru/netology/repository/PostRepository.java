3662eba2bc4e9472319a685c49f44bee3e628486
Artur Belyaev <belyaev.96@mail.ru>
2023-07-09 16:05:17 +0300

    Initial commit

---
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,58 @@
+package ru.netology.repository;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+// Stub
+public class PostRepository {
+    private ConcurrentHashMap<Long, Post> rep = new ConcurrentHashMap<>();
+    private long count = 0;
+
+    public List<Post> all() {
+        return new ArrayList<>(rep.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.of(rep.get(id));
+    }
+
+
+    /*Если от клиента приходит пост с id=0, значит, это создание нового поста.
+    Вы сохраняете его в списке и присваиваете ему новый id. Достаточно хранить
+    счётчик с целым числом и увеличивать на 1 при создании каждого нового поста.
+
+  Если от клиента приходит пост с id !=0, значит, это сохранение (обновление)
+  существующего поста. Вы ищете его в списке по id и обновляете. Продумайте самостоятельно,
+  что вы будете делать, если поста с таким id не оказалось: здесь могут быть разные стратегии.
+     */
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            if (count == 0) {
+                rep.put(count++, post);
+            } else {
+                post.setId(count);
+                rep.put(count++, post);
+            }
+        } else {
+            long postCount = post.getId();
+            if (rep.containsKey(postCount)) {
+                rep.put(postCount, post);
+            } else {
+                throw new NotFoundException("Что то пошло не так...");
+            }
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        rep.remove(id);
+    }
+}
